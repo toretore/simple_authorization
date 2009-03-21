433fdcc53b37982533e267d656b83c7403fbe6d5
Tore Darell <toredarell@gmail.com>
2009-03-21 23:20:47 +0100

    Require model methods; include controller methods; add locale file to I18n load path

--- lib/simple_authorization.rb
+++ lib/simple_authorization.rb
@@ -1,1 +1,5 @@
 require 'simple_authorization/controller_methods'
+require 'simple_authorization/model_methods'
+
+ActionController::Base.send(:include, SimpleAuthorization::ControllerMethods::Application)
+I18n.load_path.unshift(File.join(File.dirname(__FILE__), '..', 'config', 'locales', 'simple_authorization.yml'))
